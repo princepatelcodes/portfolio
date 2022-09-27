5e3e421f12248294cd2c1d618b4c700eb37e4df3
Prince <prince@Princes-MacBook-Air.local>
2022-09-27 16:27:45 +0530

    Added more

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-import { Heading, Text } from '@chakra-ui/react' 
+import { Center, Heading, Image, Text, VStack } from '@chakra-ui/react' 
 
 export default function Home() {
   return (
-     <>
-     <Heading>Prince Patel</Heading>
-     <Text>I am Prince</Text>
-     </>
+    <>
+      <Center>
+        <VStack>
+          <Heading>Prince Patel</Heading>
+          <Text>Finally Added my Website to github page </Text>
+          <Image src="https://c.tenor.com/918EwUygx1IAAAAC/mission-impossible-we-got-this.gif" alt='Success' />
+        </VStack>
+      </Center>
+    </>
   )
 }
