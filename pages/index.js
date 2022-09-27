e76e666f88c2637bd0eaca25b826401c01309e08
Prince <prince@Princes-MacBook-Air.local>
2022-09-27 16:19:50 +0530

    Added chakra ui

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Heading, Text } from '@chakra-ui/react' 
+
+export default function Home() {
+  return (
+     <>
+     <Heading>Prince Patel</Heading>
+     <Text>I am Prince</Text>
+     </>
+  )
+}
