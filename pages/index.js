1a4dfe30e541dd0770e14647f64936ffb4dcd0d4
Prince <iprincepatel22@gmail.com>
2023-02-06 23:44:22 +0530

    SYNC FROM LAPTOP

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Box, Center, Heading, Image, Text, VStack } from '@chakra-ui/react'
+import { Box, Center, Container, Flex, Heading, Image, Text, VStack } from '@chakra-ui/react'
 import Blogs from '../components/Blogs'
 import Navbar from '../components/Navbar'
 import Education from '../components/Education'
@@ -9,13 +9,29 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
+
+
+    <Container width={['100%','100%','75%','75%']} mx='auto'>
+
+      <Flex bg='whatsapp.100' my='12' data-aos="fade-up" data-aos-duration='1000' shadow='sm' p='12' rounded='2xl' flexDirection={['column', 'column', 'row', 'row']} justifyContent='space-between' alignItems='center'>
+        <Box width={['80%', '80%', '40%', '40%']}>
+          <Image alt='Live test' src='/test.png' width='100' />
+        </Box>
+        <Box width={['100%', '100%', '50%', '50%']}>
+          <Heading textAlign={['center', 'center', 'end', 'end']}>Join My Journey of Building a Product</Heading>
+          <Text py='2' textAlign={['center', 'center', 'end', 'end']}> This is where I share Logs about</Text>
+        </Box>
+      </Flex>
+    </Container>
+
+
       <Box id='education'></Box>
       <VStack my='2'>
         <Heading fontSize='4xl' fontWeight='800'>My Education</Heading>
         <Text>Here is my educational journey 😇.</Text>
         <Box id='blogs'></Box>
         <Education />
-        <Blogs />
+        {/* <Blogs /> */}
       </VStack>
     </>
   )
