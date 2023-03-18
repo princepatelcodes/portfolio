7fa252b7deb419c2b727a3d46fa9e6ffafcbee8e
Prince <iprincepatel22@gmail.com>
2023-03-18 19:39:33 +0530

    Re Factor

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import { Box, Center, Container, Flex, Heading, Image, Text, VStack } from '@chakra-ui/react'
-import Blogs from '../components/Blogs'
+import { Box, Container, Flex, Heading, Image, Text, VStack } from '@chakra-ui/react'
 import Navbar from '../components/Navbar'
 import Education from '../components/Education'
 import Hero from '../components/Hero'
@@ -11,18 +10,18 @@ export default function Home() {
       <Hero />
 
 
-    <Container width={['100%','100%','75%','75%']} mx='auto'>
+      <Container width={['100%', '100%', '75%', '75%']} mx='auto'>
 
-      <Flex bg='whatsapp.100' my='12' data-aos="fade-up" data-aos-duration='1000' shadow='sm' p='12' rounded='2xl' flexDirection={['column', 'column', 'row', 'row']} justifyContent='space-between' alignItems='center'>
-        <Box width={['80%', '80%', '40%', '40%']}>
-          <Image alt='Live test' src='/test.png' width='100' />
-        </Box>
-        <Box width={['100%', '100%', '50%', '50%']}>
-          <Heading textAlign={['center', 'center', 'end', 'end']}>Join My Journey of Building a Product</Heading>
-          <Text py='2' textAlign={['center', 'center', 'end', 'end']}> This is where I share Logs about</Text>
-        </Box>
-      </Flex>
-    </Container>
+        <Flex bg='whatsapp.100' my='12' data-aos="fade-up" data-aos-duration='1000' shadow='sm' p='12' rounded='2xl' flexDirection={['column', 'column', 'row', 'row']} justifyContent='space-between' alignItems='center'>
+          <Box width={['80%', '80%', '40%', '40%']}>
+            <Image alt='Live test' src='/test.png' width='100' />
+          </Box>
+          <Box width={['100%', '100%', '50%', '50%']}>
+            <Heading textAlign={['center', 'center', 'end', 'end']}>Join My Journey of Building a Product</Heading>
+            <Text py='2' textAlign={['center', 'center', 'end', 'end']}> This is where I share Logs about</Text>
+          </Box>
+        </Flex>
+      </Container>
 
 
       <Box id='education'></Box>
